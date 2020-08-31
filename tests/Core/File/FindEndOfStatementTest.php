599f885a7aa0e4c17327c585e57a376a422282e0
jrfnl <jrfnl@users.noreply.github.com>
2020-08-31 18:55:35 +0200

    File::find[Start|End]OfStatement(): update tests to allow for PHP 8 identifier tokens

--- tests/Core/File/FindEndOfStatementTest.php
+++ tests/Core/File/FindEndOfStatementTest.php
@@ -196,7 +196,7 @@ final class FindEndOfStatementTest extends AbstractMethodTestCase
         $start = $this->getTargetToken('/* testUseGroup */', T_USE);
         $found = self::$phpcsFile->findEndOfStatement($start);
 
-        $this->assertSame(($start + 23), $found);
+        $this->assertSame(($start + 21), $found);
 
     }//end testUseGroup()
 
