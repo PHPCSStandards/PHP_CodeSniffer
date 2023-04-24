d0b591ef3faf31b20cb2de5ba1c75152b9f388e8
jrfnl <jrfnl@users.noreply.github.com>
2023-04-24 21:25:45 +0200

    File: add `getListenerTimes()` method
    ... to allow for access to the recorded listener times from within a report class.

--- src/Files/File.php
+++ src/Files/File.php
@@ -204,6 +204,7 @@ class File
      * An array of sniffs being processed and how long they took.
      *
      * @var array
+     * @see getListenerTimes()
      */
     protected $listenerTimes = [];
 
@@ -1176,6 +1177,18 @@ class File
         return $this->metrics;
 
     }//end getMetrics()
+
+
+    /**
+     * Returns the time taken processing this file for each invoked sniff.
+     *
+     * @return array
+     */
+    public function getListenerTimes()
+    {
+        return $this->listenerTimes;
+
+    }//end getListenerTimes()
 
 
     /**
