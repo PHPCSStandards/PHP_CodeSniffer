ad5036ac8c2790addc7236c1f65248bf11f7d04d
jrfnl <jrfnl@users.noreply.github.com>
2023-08-04 19:32:24 +0200

    Generic/LowerCaseConstant: add tests with functions which don't create scope

--- src/Standards/Generic/Tests/PHP/LowerCaseConstantUnitTest.php
+++ src/Standards/Generic/Tests/PHP/LowerCaseConstantUnitTest.php
@@ -47,6 +47,8 @@ class LowerCaseConstantUnitTest extends AbstractSniffUnitTest
             94  => 2,
             95  => 1,
             100 => 2,
+            104 => 1,
+            108 => 1,
         ];
 
     }//end getErrorList()
