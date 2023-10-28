acd5ebce247e9ba1d3f7f36368f0fcd60d8a8958
jrfnl <jrfnl@users.noreply.github.com>
2023-10-28 21:00:45 +0200

    Runner::processFile(): remove stray passed params
    The `Reporter::cacheFileReport()` method only takes one parameter.

--- src/Runner.php
+++ src/Runner.php
@@ -688,7 +688,7 @@ class Runner
             $file->addErrorOnLine($error, 1, 'Internal.Exception');
         }//end try
 
-        $this->reporter->cacheFileReport($file, $this->config);
+        $this->reporter->cacheFileReport($file);
 
         if ($this->config->interactive === true) {
             /*
@@ -723,7 +723,7 @@ class Runner
                     $file->ruleset->populateTokenListeners();
                     $file->reloadContent();
                     $file->process();
-                    $this->reporter->cacheFileReport($file, $this->config);
+                    $this->reporter->cacheFileReport($file);
                     break;
                 }
             }//end while
