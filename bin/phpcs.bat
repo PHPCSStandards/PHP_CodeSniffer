baaaa41d6f00e3c44fe0f67425b7cd65e7ffd54d
jrfnl <jrfnl@users.noreply.github.com>
2023-11-09 13:29:16 +0100

    More clean up after removal of PEAR installation support
    Found another few references which should be removed.

--- bin/phpcs.bat
+++ bin/phpcs.bat
@@ -5,8 +5,6 @@ REM @author    Greg Sherwood <gsherwood@squiz.net>
 REM @copyright 2006-2015 Squiz Pty Ltd (ABN 77 084 670 600)
 REM @license   https://github.com/PHPCSStandards/PHP_CodeSniffer/blob/master/licence.txt BSD Licence
 
-if "%PHP_PEAR_PHP_BIN%" neq "" (
-    set PHPBIN=%PHP_PEAR_PHP_BIN%
-) else set PHPBIN=php
+set PHPBIN=php
 
 "%PHPBIN%" "%~dp0\phpcs" %*
