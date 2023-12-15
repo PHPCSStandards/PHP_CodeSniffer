7c29f488e00f09d3ec6db71be14974a4d85c920b
Rodrigo Primo <rodrigosprimo@gmail.com>
2023-12-15 10:13:28 -0300

    Replace default value of first parameter in getWarningList() with ''
    Replacing the default value with an empty string. Per Juliette's comment
    (https://github.com/PHPCSStandards/PHP_CodeSniffer/pull/163#discussion_r1426775588)
    there are three reasons why we want to do that:
    
    - The default value has no value in practice. It is an optional argument which is not enforced via the abstract functions, but is passed in all cases, so the default is never used in practice.
    - Setting the default value as file 1 also has an assumption implied, while IMO assumptions have no place in a test suite.
    - Maintainability - one less thing to have to keep in sync and to guard against typos

--- src/Standards/PEAR/Tests/Commenting/FileCommentUnitTest.php
+++ src/Standards/PEAR/Tests/Commenting/FileCommentUnitTest.php
@@ -74,7 +74,7 @@ class FileCommentUnitTest extends AbstractSniffUnitTest
      *
      * @return array<int, int>
      */
-    public function getWarningList($testFile='FileCommentUnitTest.inc')
+    public function getWarningList($testFile='')
     {
         switch ($testFile) {
         case 'FileCommentUnitTest.1.inc':
