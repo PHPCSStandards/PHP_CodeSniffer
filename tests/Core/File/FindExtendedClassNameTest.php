f627f0e58e2c4bbd399707b2905d6f275020c91a
jrfnl <jrfnl@users.noreply.github.com>
2023-12-29 09:11:24 +0100

    Tests/FindExtendedClassNameTest: use named data sets
    With non-named data sets, when a test fails, PHPUnit will display the number of the test which failed.
    
    With tests which have a _lot_ of data sets, this makes it _interesting_ (and time-consuming) to debug those, as one now has to figure out which of the data sets in the data provider corresponds to that number.
    
    Using named data sets makes debugging failing tests more straight forward as PHPUnit will display the data set name instead of the number.
    Using named data sets also documents what exactly each data set is testing.
    
    Aside from adding the data set name, this commit also adds the parameter name for each item in the data set, this time in an effort to make it more straight forward to update and add tests as it will be more obvious what each key in the data set signifies.
    
    Includes fixing the data types in the docblocks and making them more specific, where relevant.

--- tests/Core/File/FindExtendedClassNameTest.php
+++ tests/Core/File/FindExtendedClassNameTest.php
@@ -24,8 +24,8 @@ class FindExtendedClassNameTest extends AbstractMethodUnitTest
      * Test retrieving the name of the class being extended by another class
      * (or interface).
      *
-     * @param string $identifier Comment which precedes the test case.
-     * @param bool   $expected   Expected function output.
+     * @param string       $identifier Comment which precedes the test case.
+     * @param string|false $expected   Expected function output.
      *
      * @dataProvider dataExtendedClass
      *
@@ -45,50 +45,50 @@ class FindExtendedClassNameTest extends AbstractMethodUnitTest
      *
      * @see testFindExtendedClassName()
      *
-     * @return array
+     * @return array<string, array<string, string|false>>
      */
     public function dataExtendedClass()
     {
         return [
-            [
-                '/* testExtendsUnqualifiedClass */',
-                'testFECNClass',
+            'class extends unqualified class'                             => [
+                'identifier' => '/* testExtendsUnqualifiedClass */',
+                'expected'   => 'testFECNClass',
             ],
-            [
-                '/* testExtendsFullyQualifiedClass */',
-                '\PHP_CodeSniffer\Tests\Core\File\testFECNClass',
+            'class extends fully qualified class'                         => [
+                'identifier' => '/* testExtendsFullyQualifiedClass */',
+                'expected'   => '\PHP_CodeSniffer\Tests\Core\File\testFECNClass',
             ],
-            [
-                '/* testNonExtendedClass */',
-                false,
+            'class does not extend'                                       => [
+                'identifier' => '/* testNonExtendedClass */',
+                'expected'   => false,
             ],
-            [
-                '/* testNonExtendedInterface */',
-                false,
+            'interface does not extend'                                   => [
+                'identifier' => '/* testNonExtendedInterface */',
+                'expected'   => false,
             ],
-            [
-                '/* testInterfaceExtendsUnqualifiedInterface */',
-                'testFECNInterface',
+            'interface extends unqualified interface'                     => [
+                'identifier' => '/* testInterfaceExtendsUnqualifiedInterface */',
+                'expected'   => 'testFECNInterface',
             ],
-            [
-                '/* testInterfaceExtendsFullyQualifiedInterface */',
-                '\PHP_CodeSniffer\Tests\Core\File\testFECNInterface',
+            'interface extends fully qualified interface'                 => [
+                'identifier' => '/* testInterfaceExtendsFullyQualifiedInterface */',
+                'expected'   => '\PHP_CodeSniffer\Tests\Core\File\testFECNInterface',
             ],
-            [
-                '/* testNestedExtendedClass */',
-                false,
+            'class does not extend but contains anon class which extends' => [
+                'identifier' => '/* testNestedExtendedClass */',
+                'expected'   => false,
             ],
-            [
-                '/* testNestedExtendedAnonClass */',
-                'testFECNAnonClass',
+            'anon class extends, nested in non-extended class'            => [
+                'identifier' => '/* testNestedExtendedAnonClass */',
+                'expected'   => 'testFECNAnonClass',
             ],
-            [
-                '/* testClassThatExtendsAndImplements */',
-                'testFECNClass',
+            'class extends and implements'                                => [
+                'identifier' => '/* testClassThatExtendsAndImplements */',
+                'expected'   => 'testFECNClass',
             ],
-            [
-                '/* testClassThatImplementsAndExtends */',
-                'testFECNClass',
+            'class implements and extends'                                => [
+                'identifier' => '/* testClassThatImplementsAndExtends */',
+                'expected'   => 'testFECNClass',
             ],
         ];
 
