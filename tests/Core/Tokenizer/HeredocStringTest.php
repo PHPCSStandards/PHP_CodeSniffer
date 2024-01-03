4298671cfed5ba1f5642051f73de51f03f1ac88a
jrfnl <jrfnl@users.noreply.github.com>
2024-01-03 11:57:12 +0100

    Tests/HeredocStringTest: use named data sets
    With non-named data sets, when a test fails, PHPUnit will display the number of the test which failed.
    
    With tests which have a _lot_ of data sets, this makes it _interesting_ (and time-consuming) to debug those, as one now has to figure out which of the data sets in the data provider corresponds to that number.
    
    Using named data sets makes debugging failing tests more straight forward as PHPUnit will display the data set name instead of the number.
    Using named data sets also documents what exactly each data set is testing.
    
    Includes making the data type in the docblock more specific.

--- tests/Core/Tokenizer/HeredocStringTest.php
+++ tests/Core/Tokenizer/HeredocStringTest.php
@@ -62,86 +62,94 @@ final class HeredocStringTest extends AbstractMethodUnitTest
     /**
      * Data provider.
      *
+     * Type reference:
+     * 1. Directly embedded variables.
+     * 2. Braces outside the variable.
+     * 3. Braces after the dollar sign.
+     * 4. Variable variables and expressions.
+     *
+     * @link https://wiki.php.net/rfc/deprecate_dollar_brace_string_interpolation
+     *
      * @see testHeredocString()
      *
-     * @return array
+     * @return array<string, array<string, string>>
      */
     public static function dataHeredocString()
     {
         return [
-            [
+            'Type 1: simple variable'                                                  => [
                 'testMarker'      => '/* testSimple1 */',
                 'expectedContent' => '$foo',
             ],
-            [
+            'Type 2: simple variable'                                                  => [
                 'testMarker'      => '/* testSimple2 */',
                 'expectedContent' => '{$foo}',
             ],
-            [
+            'Type 3: simple variable'                                                  => [
                 'testMarker'      => '/* testSimple3 */',
                 'expectedContent' => '${foo}',
             ],
-            [
+            'Type 1: array offset'                                                     => [
                 'testMarker'      => '/* testDIM1 */',
                 'expectedContent' => '$foo[bar]',
             ],
-            [
+            'Type 2: array offset'                                                     => [
                 'testMarker'      => '/* testDIM2 */',
                 'expectedContent' => '{$foo[\'bar\']}',
             ],
-            [
+            'Type 3: array offset'                                                     => [
                 'testMarker'      => '/* testDIM3 */',
                 'expectedContent' => '${foo[\'bar\']}',
             ],
-            [
+            'Type 1: object property'                                                  => [
                 'testMarker'      => '/* testProperty1 */',
                 'expectedContent' => '$foo->bar',
             ],
-            [
+            'Type 2: object property'                                                  => [
                 'testMarker'      => '/* testProperty2 */',
                 'expectedContent' => '{$foo->bar}',
             ],
-            [
+            'Type 2: object method call'                                               => [
                 'testMarker'      => '/* testMethod1 */',
                 'expectedContent' => '{$foo->bar()}',
             ],
-            [
+            'Type 2: closure function call'                                            => [
                 'testMarker'      => '/* testClosure1 */',
                 'expectedContent' => '{$foo()}',
             ],
-            [
+            'Type 2: chaining various syntaxes'                                        => [
                 'testMarker'      => '/* testChain1 */',
                 'expectedContent' => '{$foo[\'bar\']->baz()()}',
             ],
-            [
+            'Type 4: variable variables'                                               => [
                 'testMarker'      => '/* testVariableVar1 */',
                 'expectedContent' => '${$bar}',
             ],
-            [
+            'Type 4: variable constants'                                               => [
                 'testMarker'      => '/* testVariableVar2 */',
                 'expectedContent' => '${(foo)}',
             ],
-            [
+            'Type 4: object property'                                                  => [
                 'testMarker'      => '/* testVariableVar3 */',
                 'expectedContent' => '${foo->bar}',
             ],
-            [
+            'Type 4: variable variable nested in array offset'                         => [
                 'testMarker'      => '/* testNested1 */',
                 'expectedContent' => '${foo["${bar}"]}',
             ],
-            [
+            'Type 4: variable array offset nested in array offset'                     => [
                 'testMarker'      => '/* testNested2 */',
                 'expectedContent' => '${foo["${bar[\'baz\']}"]}',
             ],
-            [
+            'Type 4: variable object property'                                         => [
                 'testMarker'      => '/* testNested3 */',
                 'expectedContent' => '${foo->{$baz}}',
             ],
-            [
+            'Type 4: variable object property - complex with single quotes'            => [
                 'testMarker'      => '/* testNested4 */',
                 'expectedContent' => '${foo->{${\'a\'}}}',
             ],
-            [
+            'Type 4: variable object property - complex with single and double quotes' => [
                 'testMarker'      => '/* testNested5 */',
                 'expectedContent' => '${foo->{"${\'a\'}"}}',
             ],
