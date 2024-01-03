01e354e49beeb4e2c9a882fed0d6333de8626b52
jrfnl <jrfnl@users.noreply.github.com>
2024-01-03 10:07:00 +0100

    Tests/HeredocNowdocCloserTest: remove overloaded "setUpBeforeClass" method
    Follow up on 220 / commit 5aa4362eb589e1290c1e23f54e9393a2b26390c5 .
    
    As the `tabWidth` setting is now available via the `AbstractMethodUnitTest`, the `initializeFile()` method no longer needs to be overloaded for this test.

--- tests/Core/Tokenizer/HeredocNowdocCloserTest.php
+++ tests/Core/Tokenizer/HeredocNowdocCloserTest.php
@@ -9,9 +9,6 @@
 
 namespace PHP_CodeSniffer\Tests\Core\Tokenizer;
 
-use PHP_CodeSniffer\Config;
-use PHP_CodeSniffer\Ruleset;
-use PHP_CodeSniffer\Files\DummyFile;
 use PHP_CodeSniffer\Tests\Core\AbstractMethodUnitTest;
 
 /**
@@ -21,37 +18,6 @@ use PHP_CodeSniffer\Tests\Core\AbstractMethodUnitTest;
  */
 final class HeredocNowdocCloserTest extends AbstractMethodUnitTest
 {
-
-
-    /**
-     * Initialize & tokenize \PHP_CodeSniffer\Files\File with code from the test case file.
-     *
-     * {@internal This is a near duplicate of the original method. Only difference is that
-     * tab replacement is enabled for this test.}
-     *
-     * @return void
-     */
-    public static function setUpBeforeClass(): void
-    {
-        $config            = new Config();
-        $config->standards = ['PSR1'];
-        $config->tabWidth  = 4;
-
-        $ruleset = new Ruleset($config);
-
-        // Default to a file with the same name as the test class. Extension is property based.
-        $relativeCN     = str_replace(__NAMESPACE__, '', get_called_class());
-        $relativePath   = str_replace('\\', DIRECTORY_SEPARATOR, $relativeCN);
-        $pathToTestFile = realpath(__DIR__).$relativePath.'.'.static::$fileExtension;
-
-        // Make sure the file gets parsed correctly based on the file type.
-        $contents  = 'phpcs_input_file: '.$pathToTestFile.PHP_EOL;
-        $contents .= file_get_contents($pathToTestFile);
-
-        self::$phpcsFile = new DummyFile($contents, $ruleset, $config);
-        self::$phpcsFile->process();
-
-    }//end setUpBeforeClass()
 
 
     /**
