0bc8a3f1b6e8bf47745211b251b87c08a1d5d88d
Przemysław Hernik <przemek.hernik@hotmail.com>
2024-01-28 22:19:17 +0100

    add sniff codes to docFiles array

--- src/Generators/Generator.php
+++ src/Generators/Generator.php
@@ -14,6 +14,7 @@ namespace PHP_CodeSniffer\Generators;
 
 use PHP_CodeSniffer\Ruleset;
 use PHP_CodeSniffer\Autoload;
+use PHP_CodeSniffer\Util;
 
 abstract class Generator
 {
@@ -52,9 +53,10 @@ abstract class Generator
                 $file
             );
             $docFile = str_replace('Sniff.php', 'Standard.xml', $docFile);
+            $sniffCode = Util\Common::getSniffCode($className);
 
             if (is_file($docFile) === true) {
-                $this->docFiles[] = $docFile;
+                $this->docFiles[$sniffCode] = $docFile;
             }
         }
 
@@ -89,7 +91,7 @@ abstract class Generator
      */
     public function generate()
     {
-        foreach ($this->docFiles as $file) {
+        foreach ($this->docFiles as $code => $file) {
             $doc = new \DOMDocument();
             $doc->load($file);
             $documentation = $doc->getElementsByTagName('documentation')->item(0);
