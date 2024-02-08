bc781f039cf75d8d50cf9aa07edf42c0586ecc37
Dan Wallis <dan@wallis.nz>
2024-02-08 13:21:36 +0000

    PSR12/DeclareStatement - handle parse error

--- src/Standards/PSR12/Sniffs/Files/DeclareStatementSniff.php
+++ src/Standards/PSR12/Sniffs/Files/DeclareStatementSniff.php
@@ -110,6 +110,12 @@ class DeclareStatementSniff implements Sniff
 
         // There should be no space between equal sign and directive value.
         $value = $phpcsFile->findNext(T_WHITESPACE, ($equals + 1), null, true);
+
+        if ($value === false) {
+            // Live coding / parse error.
+            return;
+        }
+
         if ($equals !== false) {
             if ($tokens[($equals + 1)]['type'] !== 'T_LNUMBER') {
                 $error = 'Expected no space between equal sign and the directive value in a declare statement';
