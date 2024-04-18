8c09939c1ce977bd8cdd0eae037cbc2237d49d87
Rodrigo Primo <rodrigosprimo@gmail.com>
2024-04-18 14:45:44 -0300

    Generic/DisallowLongArraySyntax: simplify if statement
    This commit uses a single isset() to check if two variables are set
    instead of using two isset() calls.

--- src/Standards/Generic/Sniffs/Arrays/DisallowLongArraySyntaxSniff.php
+++ src/Standards/Generic/Sniffs/Arrays/DisallowLongArraySyntaxSniff.php
@@ -45,9 +45,7 @@ class DisallowLongArraySyntaxSniff implements Sniff
 
         $error = 'Short array syntax must be used to define arrays';
 
-        if (isset($tokens[$stackPtr]['parenthesis_opener']) === false
-            || isset($tokens[$stackPtr]['parenthesis_closer']) === false
-        ) {
+        if (isset($tokens[$stackPtr]['parenthesis_opener'], $tokens[$stackPtr]['parenthesis_closer']) === false) {
             // Live coding/parse error, just show the error, don't try and fix it.
             $phpcsFile->addError($error, $stackPtr, 'Found');
             return;
