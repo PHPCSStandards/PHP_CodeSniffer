48dfbc964b5d9a01135839f24623b482ed490c98
jrfnl <jrfnl@users.noreply.github.com>
2024-05-15 11:09:29 +0200

    Tests/Tokenizer/DefaultKeywordTest: split the test class
    ... into two test classes, one targetting the `Tokenizer\PHP` class, one targetting the `Tokenizer\Tokenizer` class.
    
    While this does mean there is now some duplication between these test classes, I don't think that's problematic.
    
    It also allows for these tests to diverge based on the specific test needs for each of the classes under test.

--- tests/Core/Tokenizer/PHP/DefaultKeywordTest.php
+++ tests/Core/Tokenizer/PHP/DefaultKeywordTest.php
@@ -8,7 +8,9 @@
  * @license   https://github.com/PHPCSStandards/PHP_CodeSniffer/blob/master/licence.txt BSD Licence
  */
 
-namespace PHP_CodeSniffer\Tests\Core\Tokenizer;
+namespace PHP_CodeSniffer\Tests\Core\Tokenizer\PHP;
+
+use PHP_CodeSniffer\Tests\Core\Tokenizer\AbstractTokenizerTestCase;
 
 final class DefaultKeywordTest extends AbstractTokenizerTestCase
 {
@@ -25,7 +27,6 @@ final class DefaultKeywordTest extends AbstractTokenizerTestCase
      *
      * @dataProvider dataMatchDefault
      * @covers       PHP_CodeSniffer\Tokenizers\PHP::tokenize
-     * @covers       PHP_CodeSniffer\Tokenizers\Tokenizer::recurseScopeMap
      *
      * @return void
      */
@@ -38,10 +39,6 @@ final class DefaultKeywordTest extends AbstractTokenizerTestCase
 
         $this->assertSame(T_MATCH_DEFAULT, $tokenArray['code'], 'Token tokenized as '.$tokenArray['type'].', not T_MATCH_DEFAULT (code)');
         $this->assertSame('T_MATCH_DEFAULT', $tokenArray['type'], 'Token tokenized as '.$tokenArray['type'].', not T_MATCH_DEFAULT (type)');
-
-        $this->assertArrayNotHasKey('scope_condition', $tokenArray, 'Scope condition is set');
-        $this->assertArrayNotHasKey('scope_opener', $tokenArray, 'Scope opener is set');
-        $this->assertArrayNotHasKey('scope_closer', $tokenArray, 'Scope closer is set');
 
     }//end testMatchDefault()
 
@@ -105,69 +102,23 @@ final class DefaultKeywordTest extends AbstractTokenizerTestCase
      * Verify that the retokenization of `T_DEFAULT` tokens in match constructs, doesn't negatively
      * impact the tokenization of `T_DEFAULT` tokens in switch control structures.
      *
-     * Note: Cases and default structures within a switch control structure *do* get case/default scope
-     * conditions.
-     *
-     * @param string   $testMarker    The comment prefacing the target token.
-     * @param int      $openerOffset  The expected offset of the scope opener in relation to the testMarker.
-     * @param int      $closerOffset  The expected offset of the scope closer in relation to the testMarker.
-     * @param int|null $conditionStop The expected offset at which tokens stop having T_DEFAULT as a scope condition.
-     * @param string   $testContent   The token content to look for.
+     * @param string $testMarker  The comment prefacing the target token.
+     * @param string $testContent The token content to look for.
      *
      * @dataProvider dataSwitchDefault
-     * @covers       PHP_CodeSniffer\Tokenizers\Tokenizer::recurseScopeMap
+     * @covers       PHP_CodeSniffer\Tokenizers\PHP::tokenize
      *
      * @return void
      */
-    public function testSwitchDefault($testMarker, $openerOffset, $closerOffset, $conditionStop=null, $testContent='default')
+    public function testSwitchDefault($testMarker, $testContent='default')
     {
         $tokens = $this->phpcsFile->getTokens();
 
         $token      = $this->getTargetToken($testMarker, [T_MATCH_DEFAULT, T_DEFAULT, T_STRING], $testContent);
         $tokenArray = $tokens[$token];
-        $expectedScopeOpener = ($token + $openerOffset);
-        $expectedScopeCloser = ($token + $closerOffset);
 
         $this->assertSame(T_DEFAULT, $tokenArray['code'], 'Token tokenized as '.$tokenArray['type'].', not T_DEFAULT (code)');
         $this->assertSame('T_DEFAULT', $tokenArray['type'], 'Token tokenized as '.$tokenArray['type'].', not T_DEFAULT (type)');
-
-        $this->assertArrayHasKey('scope_condition', $tokenArray, 'Scope condition is not set');
-        $this->assertArrayHasKey('scope_opener', $tokenArray, 'Scope opener is not set');
-        $this->assertArrayHasKey('scope_closer', $tokenArray, 'Scope closer is not set');
-        $this->assertSame($token, $tokenArray['scope_condition'], 'Scope condition is not the T_DEFAULT token');
-        $this->assertSame($expectedScopeOpener, $tokenArray['scope_opener'], 'Scope opener of the T_DEFAULT token incorrect');
-        $this->assertSame($expectedScopeCloser, $tokenArray['scope_closer'], 'Scope closer of the T_DEFAULT token incorrect');
-
-        $opener = $tokenArray['scope_opener'];
-        $this->assertArrayHasKey('scope_condition', $tokens[$opener], 'Opener scope condition is not set');
-        $this->assertArrayHasKey('scope_opener', $tokens[$opener], 'Opener scope opener is not set');
-        $this->assertArrayHasKey('scope_closer', $tokens[$opener], 'Opener scope closer is not set');
-        $this->assertSame($token, $tokens[$opener]['scope_condition'], 'Opener scope condition is not the T_DEFAULT token');
-        $this->assertSame($expectedScopeOpener, $tokens[$opener]['scope_opener'], 'T_DEFAULT opener scope opener token incorrect');
-        $this->assertSame($expectedScopeCloser, $tokens[$opener]['scope_closer'], 'T_DEFAULT opener scope closer token incorrect');
-
-        $closer = $tokenArray['scope_closer'];
-        $this->assertArrayHasKey('scope_condition', $tokens[$closer], 'Closer scope condition is not set');
-        $this->assertArrayHasKey('scope_opener', $tokens[$closer], 'Closer scope opener is not set');
-        $this->assertArrayHasKey('scope_closer', $tokens[$closer], 'Closer scope closer is not set');
-        $this->assertSame($token, $tokens[$closer]['scope_condition'], 'Closer scope condition is not the T_DEFAULT token');
-        $this->assertSame($expectedScopeOpener, $tokens[$closer]['scope_opener'], 'T_DEFAULT closer scope opener token incorrect');
-        $this->assertSame($expectedScopeCloser, $tokens[$closer]['scope_closer'], 'T_DEFAULT closer scope closer token incorrect');
-
-        if (($opener + 1) !== $closer) {
-            $end = $closer;
-            if (isset($conditionStop) === true) {
-                $end = $conditionStop;
-            }
-
-            for ($i = ($opener + 1); $i < $end; $i++) {
-                $this->assertArrayHasKey(
-                    $token,
-                    $tokens[$i]['conditions'],
-                    'T_DEFAULT condition not added for token belonging to the T_DEFAULT structure'
-                );
-            }
-        }
 
     }//end testSwitchDefault()
 
@@ -183,34 +134,19 @@ final class DefaultKeywordTest extends AbstractTokenizerTestCase
     {
         return [
             'simple_switch_default'                  => [
-                'testMarker'   => '/* testSimpleSwitchDefault */',
-                'openerOffset' => 1,
-                'closerOffset' => 4,
+                'testMarker' => '/* testSimpleSwitchDefault */',
             ],
             'simple_switch_default_with_curlies'     => [
-                // For a default structure with curly braces, the scope opener
-                // will be the open curly and the closer the close curly.
-                // However, scope conditions will not be set for open to close,
-                // but only for the open token up to the "break/return/continue" etc.
-                'testMarker'    => '/* testSimpleSwitchDefaultWithCurlies */',
-                'openerOffset'  => 3,
-                'closerOffset'  => 12,
-                'conditionStop' => 6,
+                'testMarker' => '/* testSimpleSwitchDefaultWithCurlies */',
             ],
             'switch_default_toplevel'                => [
-                'testMarker'   => '/* testSwitchDefault */',
-                'openerOffset' => 1,
-                'closerOffset' => 43,
+                'testMarker' => '/* testSwitchDefault */',
             ],
             'switch_default_nested_in_match_case'    => [
-                'testMarker'   => '/* testSwitchDefaultNestedInMatchCase */',
-                'openerOffset' => 1,
-                'closerOffset' => 20,
+                'testMarker' => '/* testSwitchDefaultNestedInMatchCase */',
             ],
             'switch_default_nested_in_match_default' => [
-                'testMarker'   => '/* testSwitchDefaultNestedInMatchDefault */',
-                'openerOffset' => 1,
-                'closerOffset' => 18,
+                'testMarker' => '/* testSwitchDefaultNestedInMatchDefault */',
             ],
         ];
 
@@ -239,10 +175,6 @@ final class DefaultKeywordTest extends AbstractTokenizerTestCase
 
         $this->assertSame(T_STRING, $tokenArray['code'], 'Token tokenized as '.$tokenArray['type'].', not T_STRING (code)');
         $this->assertSame('T_STRING', $tokenArray['type'], 'Token tokenized as '.$tokenArray['type'].', not T_STRING (type)');
-
-        $this->assertArrayNotHasKey('scope_condition', $tokenArray, 'Scope condition is set');
-        $this->assertArrayNotHasKey('scope_opener', $tokenArray, 'Scope opener is set');
-        $this->assertArrayNotHasKey('scope_closer', $tokenArray, 'Scope closer is set');
 
     }//end testNotDefaultKeyword()
 
@@ -318,30 +250,6 @@ final class DefaultKeywordTest extends AbstractTokenizerTestCase
         ];
 
     }//end dataNotDefaultKeyword()
-
-
-    /**
-     * Test a specific edge case where a scope opener would be incorrectly set.
-     *
-     * @link https://github.com/squizlabs/PHP_CodeSniffer/issues/3326
-     *
-     * @covers PHP_CodeSniffer\Tokenizers\PHP::tokenize
-     * @covers PHP_CodeSniffer\Tokenizers\Tokenizer::recurseScopeMap
-     *
-     * @return void
-     */
-    public function testIssue3326()
-    {
-        $tokens = $this->phpcsFile->getTokens();
-
-        $token      = $this->getTargetToken('/* testClassConstant */', [T_SEMICOLON]);
-        $tokenArray = $tokens[$token];
-
-        $this->assertArrayNotHasKey('scope_condition', $tokenArray, 'Scope condition is set');
-        $this->assertArrayNotHasKey('scope_opener', $tokenArray, 'Scope opener is set');
-        $this->assertArrayNotHasKey('scope_closer', $tokenArray, 'Scope closer is set');
-
-    }//end testIssue3326()
 
 
 }//end class
