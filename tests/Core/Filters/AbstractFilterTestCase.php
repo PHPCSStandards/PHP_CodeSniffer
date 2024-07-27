f7dadc9095a7d49374702a9fae207609c150004c
jrfnl <jrfnl@users.noreply.github.com>
2024-07-27 10:46:05 +0200

    Tests/ConfigDouble: bug fix - always reset Config statics after use
    The `Config` class uses a number of static properties, which may be updated during tests. These were previously - prior to 275 - reset to their default values in the `AbstractMethodUnitTest::resetTestFile()` method, but this reset was inadvertently removed with the reasoning that, if all tests use the `ConfigDouble`, the reset would no longer be needed as the `ConfigDouble` resets on being initialized.
    
    The flaw in this logic is that not all tests are guaranteed to use the `ConfigDouble`, which means that without the reset, the `Config` class may be left "dirty" after tests using the `ConfigDouble`, which could break tests.
    
    This commit fixes this issue by:
    * Adding a `__destruct()` method to the `ConfigDouble` class which will reset the static properties on the PHPCS native `Config` class whenever an object created from this class is destroyed.
    * Explicitly calling the `__destruct()` method from the `AbstractMethodUnitTest::reset()` method to ensure it is always run after a test has finished ("after class"), even if there would still be a lingering reference to the object.
    
    This is only a stability tweak, there are no existing tests affected by this issue at this time.

--- tests/Core/Filters/AbstractFilterTestCase.php
+++ tests/Core/Filters/AbstractFilterTestCase.php
@@ -50,6 +50,24 @@ abstract class AbstractFilterTestCase extends TestCase
 
 
     /**
+     * Clean up after finished test by resetting all static properties on the Config class to their default values.
+     *
+     * @return void
+     */
+    public static function tearDownAfterClass(): void
+    {
+        // Explicitly trigger __destruct() on the ConfigDouble to reset the Config statics.
+        // The explicit method call prevents potential stray test-local references to the $config object
+        // preventing the destructor from running the clean up (which without stray references would be
+        // automagically triggered when `self::$phpcsFile` is reset, but we can't definitively rely on that).
+        if (isset(self::$config) === true) {
+            self::$config->__destruct();
+        }
+
+    }//end tearDownAfterClass()
+
+
+    /**
      * Helper method to retrieve a mock object for a Filter class.
      *
      * The `setMethods()` method was silently deprecated in PHPUnit 9 and removed in PHPUnit 10.
