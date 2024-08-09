ae6bd698d81f8e355ce08127fefbb105e90c520f
Rodrigo Primo <rodrigosprimo@gmail.com>
2024-08-09 15:05:40 -0300

    Squiz/EmbeddedPhp: fix false positive when handling short open tags
    The content of a PHP long open tag token is `<?php ` (note
    the space after the tag). The content of a PHP short open tag is
    `<?` (no space after the tag). The sniff did not account correctly for
    this difference when checking the expected number of spaces after a
    short open tag, resulting in false positives and incorrect fixes.
    
    For example, the code below:
    
    ```
    <? echo 'one space after short open tag'; ?>
    ```
    
    Resulted in the error (there is just one space after the opening tag and
    not two, as stated in the error):
    
    ```
    Expected 1 space after opening PHP tag; 2 found (Squiz.PHP.EmbeddedPhp.SpacingAfterOpen)
    ```
    
    And the incorrect fix:
    
    ```
    <?echo 'without space after short open tag'; ?>
    ```
    
    This commit fixes this problem by changing the sniff code to consider
    that only long open tags contain a space after the tag in the `content`
    key of the token array.

--- src/Standards/Squiz/Sniffs/PHP/EmbeddedPhpSniff.php
+++ src/Standards/Squiz/Sniffs/PHP/EmbeddedPhpSniff.php
@@ -379,8 +379,11 @@ class EmbeddedPhpSniff implements Sniff
         }
 
         // Check that there is one, and only one space at the start of the statement.
-        $leadingSpace = 0;
-        if ($tokens[$stackPtr]['code'] === T_OPEN_TAG) {
+        $leadingSpace  = 0;
+        $isLongOpenTag = $tokens[$stackPtr]['code'] === T_OPEN_TAG
+                         && stripos($tokens[$stackPtr]['content'], '<?php') === 0;
+
+        if ($isLongOpenTag === true) {
             // The long open tag token in a single line tag set always contains a single space after it.
             $leadingSpace = 1;
         }
@@ -394,7 +397,7 @@ class EmbeddedPhpSniff implements Sniff
             $data  = [$leadingSpace];
             $fix   = $phpcsFile->addFixableError($error, $stackPtr, 'SpacingAfterOpen', $data);
             if ($fix === true) {
-                if ($tokens[$stackPtr]['code'] === T_OPEN_TAG) {
+                if ($isLongOpenTag === true) {
                     $phpcsFile->fixer->replaceToken(($stackPtr + 1), '');
                 } else if ($tokens[($stackPtr + 1)]['code'] === T_WHITESPACE) {
                     // Short open tag with too much whitespace.
