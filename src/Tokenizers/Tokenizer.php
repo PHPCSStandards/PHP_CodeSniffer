be0d9595de7d5384c65ee68b2aaafef60acdcacb
jrfnl <jrfnl@users.noreply.github.com>
2024-10-22 10:17:35 +0200

    Tokenizer::replaceTabsInToken(): add tests
    Add tests for the logic in the `Tokenizer::replaceTabsInToken()` method.

--- src/Tokenizers/Tokenizer.php
+++ src/Tokenizers/Tokenizer.php
@@ -620,7 +620,7 @@ abstract class Tokenizer
                 if ($content !== '') {
                     $newContent .= $content;
                     if ($checkEncoding === true) {
-                        // Not using the default encoding, so take a bit more care.
+                        // Not using ASCII encoding, so take a bit more care.
                         $oldLevel = error_reporting();
                         error_reporting(0);
                         $contentLength = iconv_strlen($content, $this->config->encoding);
