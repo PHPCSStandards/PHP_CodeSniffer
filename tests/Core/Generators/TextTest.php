5da39905c99a1ee49ae9f9846a2388f56e6b43a8
jrfnl <jrfnl@users.noreply.github.com>
2024-11-04 09:57:22 +0100

    Generators: add tests for handling <standard> element
    This adds dedicated tests for specific issues which can be encountered in a `<standard>` XML element.
    
    This initial set of tests for this documents the current behaviour. This behaviour may not always be the desired behaviour, in which case, this will be fixed in follow-up commits.

--- tests/Core/Generators/TextTest.php
+++ tests/Core/Generators/TextTest.php
@@ -125,13 +125,29 @@ final class TextTest extends TestCase
     public static function dataDocSpecifics()
     {
         return [
-            'Documentation title: case'   => [
+            'Documentation title: case'                        => [
                 'sniffs'         => 'StandardWithDocs.Content.DocumentationTitleCase',
                 'pathToExpected' => __DIR__.'/Expectations/ExpectedOutputDocumentationTitleCase.txt',
             ],
-            'Documentation title: length' => [
+            'Documentation title: length'                      => [
                 'sniffs'         => 'StandardWithDocs.Content.DocumentationTitleLength',
                 'pathToExpected' => __DIR__.'/Expectations/ExpectedOutputDocumentationTitleLength.txt',
+            ],
+            'Standard Element: blank line handling'            => [
+                'sniffs'         => 'StandardWithDocs.Content.StandardBlankLines',
+                'pathToExpected' => __DIR__.'/Expectations/ExpectedOutputStandardBlankLines.txt',
+            ],
+            'Standard Element: encoding of special characters' => [
+                'sniffs'         => 'StandardWithDocs.Content.StandardEncoding',
+                'pathToExpected' => __DIR__.'/Expectations/ExpectedOutputStandardEncoding.txt',
+            ],
+            'Standard Element: indent handling'                => [
+                'sniffs'         => 'StandardWithDocs.Content.StandardIndent',
+                'pathToExpected' => __DIR__.'/Expectations/ExpectedOutputStandardIndent.txt',
+            ],
+            'Standard Element: line wrapping'                  => [
+                'sniffs'         => 'StandardWithDocs.Content.StandardLineWrapping',
+                'pathToExpected' => __DIR__.'/Expectations/ExpectedOutputStandardLineWrapping.txt',
             ],
         ];
 
