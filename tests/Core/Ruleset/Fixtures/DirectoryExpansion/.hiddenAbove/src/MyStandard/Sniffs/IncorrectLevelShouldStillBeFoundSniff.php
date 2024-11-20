fe7233c5776f111da3ab1f519e58ca409ae55298
jrfnl <jrfnl@users.noreply.github.com>
2024-11-20 03:11:10 +0100

    Ruleset::expandSniffDirectory(): add test

---
+++ tests/Core/Ruleset/Fixtures/DirectoryExpansion/.hiddenAbove/src/MyStandard/Sniffs/IncorrectLevelShouldStillBeFoundSniff.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Test fixture.
+ *
+ * @see \PHP_CodeSniffer\Tests\Core\Ruleset\ExpandSniffDirectoryTest
+ */
+
+namespace MyStandard\Sniffs;
+
+use MyStandard\DummySniff;
+
+final class IncorrectLevelShouldStillBeFoundSniff extends DummySniff {}
