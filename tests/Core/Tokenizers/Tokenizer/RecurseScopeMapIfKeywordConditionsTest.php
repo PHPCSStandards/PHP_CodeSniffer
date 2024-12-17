268d1a5349a66d63656023fe2308c5b7217c99e2
Rodrigo Primo <rodrigosprimo@gmail.com>
2024-12-17 15:42:20 -0300

    Implement code review changes related to commit 3 (332c959)

--- tests/Core/Tokenizers/Tokenizer/RecurseScopeMapIfKeywordConditionsTest.php
+++ tests/Core/Tokenizers/Tokenizer/RecurseScopeMapIfKeywordConditionsTest.php
@@ -4,7 +4,7 @@
  *
  * @author    Juliette Reinders Folmer <phpcs_nospam@adviesenzo.nl>
  * @author    Rodrigo Primo <rodrigosprimo@gmail.com>
- * @copyright 2021 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @copyright 2024 PHPCSStandards and contributors
  * @license   https://github.com/PHPCSStandards/PHP_CodeSniffer/blob/master/licence.txt BSD Licence
  */
 
