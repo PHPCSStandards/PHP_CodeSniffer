a694a6df1c5dbb9a5d9f6515278f826d672da279
jrfnl <jrfnl@users.noreply.github.com>
2025-03-18 17:42:10 +0100

    Tests: remove some PHP < 7.2 specific code

--- src/Standards/Generic/Tests/PHP/DeprecatedFunctionsUnitTest.php
+++ src/Standards/Generic/Tests/PHP/DeprecatedFunctionsUnitTest.php
@@ -32,7 +32,7 @@ final class DeprecatedFunctionsUnitTest extends AbstractSniffUnitTest
     {
         $errors = [];
 
-        if (PHP_VERSION_ID >= 70200 && PHP_VERSION_ID < 80000) {
+        if (PHP_VERSION_ID < 80000) {
             $errors[3] = 1;
         }
 
