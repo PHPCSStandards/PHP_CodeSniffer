387063d351b9209b6df5c3a69441f0e5ea3369be
Daniel Scherzer <daniel.e.scherzer@gmail.com>
2025-04-05 21:34:30 -0700

    Nullable fix

--- src/Tokenizers/PHP.php
+++ src/Tokenizers/PHP.php
@@ -2235,6 +2235,7 @@ class PHP extends Tokenizer
                     if ($tokenType === T_FUNCTION
                         || $tokenType === T_FN
                         || isset(Tokens::$methodPrefixes[$tokenType]) === true
+                        || isset(Tokens::$scopeModifiers[$tokenType]) === true
                         || $tokenType === T_VAR
                         || $tokenType === T_READONLY
                     ) {
