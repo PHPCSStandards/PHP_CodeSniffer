71c8de23d23e41d8fc86de30bad597aac4d67808
jrfnl <jrfnl@users.noreply.github.com>
2025-04-09 01:54:58 +0200

    Squiz/DisallowComparisonAssignment: fix sniff to work with the PHP 8 identifier tokens
    Includes extra unit tests covering the change in so far it wasn't already covered.
    
    Note: this was previously a false positive for this sniff!

--- src/Standards/Squiz/Sniffs/PHP/DisallowComparisonAssignmentSniff.php
+++ src/Standards/Squiz/Sniffs/PHP/DisallowComparisonAssignmentSniff.php
@@ -68,18 +68,16 @@ class DisallowComparisonAssignmentSniff implements Sniff
         }
 
         // Ignore function calls.
-        $ignore = [
-            T_NULLSAFE_OBJECT_OPERATOR,
-            T_OBJECT_OPERATOR,
-            T_STRING,
-            T_VARIABLE,
-            T_WHITESPACE,
-        ];
+        $ignore   = Tokens::$nameTokens;
+        $ignore[] = T_NULLSAFE_OBJECT_OPERATOR;
+        $ignore[] = T_OBJECT_OPERATOR;
+        $ignore[] = T_VARIABLE;
+        $ignore[] = T_WHITESPACE;
 
         $next = $phpcsFile->findNext($ignore, ($stackPtr + 1), null, true);
         if ($tokens[$next]['code'] === T_CLOSURE
             || ($tokens[$next]['code'] === T_OPEN_PARENTHESIS
-            && $tokens[($next - 1)]['code'] === T_STRING)
+            && isset(Tokens::$nameTokens[$tokens[($next - 1)]['code']]) === true)
         ) {
             // Code will look like: $var = myFunction(
             // and will be ignored.
