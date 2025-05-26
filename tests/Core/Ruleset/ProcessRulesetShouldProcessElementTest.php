3661b1fa33e91a9798ec2a2a4755aae5ffb68356
Dan Wallis <dan@wallis.nz>
2025-05-26 11:19:29 +0100

    Declare dependency on bcmath extension
    These tests fail if the 'bcmath' PHP extension is not loaded. Elsewhere, we use
    this syntax to skip tests which depend on the 'bcmath' extension.

--- tests/Core/Ruleset/ProcessRulesetShouldProcessElementTest.php
+++ tests/Core/Ruleset/ProcessRulesetShouldProcessElementTest.php
@@ -215,6 +215,8 @@ final class ProcessRulesetShouldProcessElementTest extends TestCase
      * Verify that in CS mode, phpcs-only <ini> directives are respected and phpcbf-only <ini>
      * directives are ignored.
      *
+     * @requires extension bcmath
+     *
      * @return void
      */
     public function testShouldProcessIniCsonly()
@@ -234,7 +236,8 @@ final class ProcessRulesetShouldProcessElementTest extends TestCase
      * Verify that in CBF mode, phpcbf-only <ini> directives are respected and phpcs-only <ini>
      * directives are ignored.
      *
-     * @group CBF
+     * @group    CBF
+     * @requires extension bcmath
      *
      * @return void
      */
