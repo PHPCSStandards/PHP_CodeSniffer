2e9da03b4ce03b741bd30fdf83f5e2eb1f55b1c8
jrfnl <jrfnl@users.noreply.github.com>
2025-06-05 23:24:28 +0200

    PHP 8.4 | File::getMethodParameters(): add support for asymmetric visibility
    Implements the suggested support per https://github.com/PHPCSStandards/PHP_CodeSniffer/issues/851#issuecomment-2720976399.
    
    Includes tests.
    
    Follow up on 871
    Partial fix for 851
    
    Related to 734

--- src/Files/File.php
+++ src/Files/File.php
@@ -1354,6 +1354,10 @@ class File
      *         'readonly_token'      => integer,       // The stack pointer to the readonly modifier token.
      *                                                 // This index will only be set if the property is readonly.
      *
+     * ... and if the promoted property uses asymmetric visibility, these additional array indexes will also be available:
+     *         'set_visibility'       => string,       // The property set-visibility as declared.
+     *         'set_visibility_token' => integer,      // The stack pointer to the set-visibility modifier token.
+     *
      * @param int $stackPtr The position in the stack of the function token
      *                      to acquire the parameters for.
      *
@@ -1406,10 +1410,11 @@ class File
         $variadicToken   = false;
         $typeHint        = '';
         $typeHintToken   = false;
-        $typeHintEndToken = false;
-        $nullableType     = false;
-        $visibilityToken  = null;
-        $readonlyToken    = null;
+        $typeHintEndToken   = false;
+        $nullableType       = false;
+        $visibilityToken    = null;
+        $setVisibilityToken = null;
+        $readonlyToken      = null;
 
         for ($i = $paramStart; $i <= $closer; $i++) {
             // Check to see if this token has a parenthesis or bracket opener. If it does
@@ -1541,6 +1546,13 @@ class File
                     $visibilityToken = $i;
                 }
                 break;
+            case T_PUBLIC_SET:
+            case T_PROTECTED_SET:
+            case T_PRIVATE_SET:
+                if ($defaultStart === null) {
+                    $setVisibilityToken = $i;
+                }
+                break;
             case T_READONLY:
                 if ($defaultStart === null) {
                     $readonlyToken = $i;
@@ -1575,16 +1587,21 @@ class File
                 $vars[$paramCount]['type_hint_end_token'] = $typeHintEndToken;
                 $vars[$paramCount]['nullable_type']       = $nullableType;
 
-                if ($visibilityToken !== null || $readonlyToken !== null) {
+                if ($visibilityToken !== null || $setVisibilityToken !== null || $readonlyToken !== null) {
                     $vars[$paramCount]['property_visibility'] = 'public';
                     $vars[$paramCount]['visibility_token']    = false;
-                    $vars[$paramCount]['property_readonly']   = false;
 
                     if ($visibilityToken !== null) {
                         $vars[$paramCount]['property_visibility'] = $this->tokens[$visibilityToken]['content'];
                         $vars[$paramCount]['visibility_token']    = $visibilityToken;
                     }
 
+                    if ($setVisibilityToken !== null) {
+                        $vars[$paramCount]['set_visibility']       = $this->tokens[$setVisibilityToken]['content'];
+                        $vars[$paramCount]['set_visibility_token'] = $setVisibilityToken;
+                    }
+
+                    $vars[$paramCount]['property_readonly'] = false;
                     if ($readonlyToken !== null) {
                         $vars[$paramCount]['property_readonly'] = true;
                         $vars[$paramCount]['readonly_token']    = $readonlyToken;
@@ -1598,21 +1615,22 @@ class File
                 }
 
                 // Reset the vars, as we are about to process the next parameter.
-                $currVar          = null;
-                $paramStart       = ($i + 1);
-                $defaultStart     = null;
-                $equalToken       = null;
-                $hasAttributes    = false;
-                $passByReference  = false;
-                $referenceToken   = false;
-                $variableLength   = false;
-                $variadicToken    = false;
-                $typeHint         = '';
-                $typeHintToken    = false;
-                $typeHintEndToken = false;
-                $nullableType     = false;
-                $visibilityToken  = null;
-                $readonlyToken    = null;
+                $currVar            = null;
+                $paramStart         = ($i + 1);
+                $defaultStart       = null;
+                $equalToken         = null;
+                $hasAttributes      = false;
+                $passByReference    = false;
+                $referenceToken     = false;
+                $variableLength     = false;
+                $variadicToken      = false;
+                $typeHint           = '';
+                $typeHintToken      = false;
+                $typeHintEndToken   = false;
+                $nullableType       = false;
+                $visibilityToken    = null;
+                $setVisibilityToken = null;
+                $readonlyToken      = null;
 
                 $paramCount++;
                 break;
