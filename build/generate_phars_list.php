05b61fc210fd2ae40a91bfff004185f34f422d4e
Bernhard Zwein <52607214+benno5020@users.noreply.github.com>
2025-06-29 14:59:18 +0200

    gh-pages: make pages fancier
    The download icon is taken from
    https://github.com/feathericons/feather/blob/main/icons/download.svg
    (MIT license).
    
    The GitHub logo is taken from https://github.com/logos

---
+++ build/generate_phars_list.php
@@ -0,0 +1,70 @@
+
+<?php
+
+// A directory containing files that are named {phpcs,phpcbf}-*.{phar,phar.asc}.
+$pharDir = __DIR__ . '/../src/phars';
+
+$allFiles = array_filter(scandir($pharDir), function ($file) {
+    return preg_match('/\.phar(\.asc)?$/', $file);
+});
+
+$filesGroupedByVersion = [];
+
+foreach ($allFiles as $file) {
+    $matches = [];
+
+    if (preg_match('/^(?:phpcs|phpcbf)-(.*?)(\.phar(?:\.asc)?)$/', $file, $matches)) {
+        $version = $matches[1];
+
+        if (!isset($filesGroupedByVersion[$version])) {
+            $filesGroupedByVersion[$version] = [];
+        }
+
+        $filesGroupedByVersion[$version][] = $file;
+    }
+}
+
+uksort($filesGroupedByVersion, function ($a, $b) {
+    return version_compare($b, $a);
+});
+
+function indent (int $level): string
+{
+    $indentSpaces = '    ';
+
+    $output = "";
+
+    for ($i = 0; $i < $level; $i++) {
+        $output .= $indentSpaces;
+    }
+
+    return $output;
+}
+
+$html = "<ul class=\"phar-list\">\n";
+
+foreach ($filesGroupedByVersion as $version => $files) {
+    sort($files);
+
+    $html .= indent(3) . "<li class=\"phar-list__version\">\n" .
+    indent(4) . "<h2 class=\"phar-list__version-label\">" . $version . "</h2>\n" .
+    indent(4) . "<ul class=\"phar-list__files\">\n";
+
+    foreach ($files as $file) {
+        $html .= indent(5) . "<li><a download href=\"phars/" . htmlspecialchars($file) . '">' . htmlspecialchars($file) . "</a></li>\n";
+    }
+
+    $html .= indent(4) . "</ul>\n" .
+    indent(3) . "</li>\n";
+}
+
+$html .= indent(2) . "</li>\n" .
+         "</ul>\n";
+
+$template = file_get_contents(__DIR__ . '/phars.html.template');
+
+$output = str_replace('<!-- {{ PHARS }} -->', $html, $template);
+
+file_put_contents($pharDir . '/index.html', $output);
+
+echo $pharDir . "/index.html generated successfully.\n";
