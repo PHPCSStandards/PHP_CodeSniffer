db4f766eb8bb86a7a51797bfb0b7bcfb7bad7a56
jrfnl <jrfnl@users.noreply.github.com>
2025-08-06 17:30:21 +0200

    E2E tests: rename PHPCS config fixture
    ... to prevent it from ever being confused with the real project ruleset.

--- tests/EndToEnd/phpcbf_test.sh
+++ tests/EndToEnd/phpcbf_test.sh
@@ -5,21 +5,21 @@ function tear_down() {
 }
 
 function test_phpcbf_is_working() {
-  OUTPUT="$(bin/phpcbf --no-cache --standard=tests/EndToEnd/Fixtures/phpcs.xml.dist tests/EndToEnd/Fixtures/ClassOneWithoutStyleError.inc tests/EndToEnd/Fixtures/ClassTwoWithoutStyleError.inc)"
+  OUTPUT="$(bin/phpcbf --no-cache --standard=tests/EndToEnd/Fixtures/endtoend.xml.dist tests/EndToEnd/Fixtures/ClassOneWithoutStyleError.inc tests/EndToEnd/Fixtures/ClassTwoWithoutStyleError.inc)"
 
   assert_successful_code
   assert_contains "No violations were found" "$OUTPUT"
 }
 
 function test_phpcbf_is_working_in_parallel() {
-  OUTPUT="$(bin/phpcbf --no-cache --parallel=2 --standard=tests/EndToEnd/Fixtures/phpcs.xml.dist tests/EndToEnd/Fixtures/ClassOneWithoutStyleError.inc tests/EndToEnd/Fixtures/ClassTwoWithoutStyleError.inc)"
+  OUTPUT="$(bin/phpcbf --no-cache --parallel=2 --standard=tests/EndToEnd/Fixtures/endtoend.xml.dist tests/EndToEnd/Fixtures/ClassOneWithoutStyleError.inc tests/EndToEnd/Fixtures/ClassTwoWithoutStyleError.inc)"
 
   assert_successful_code
   assert_contains "No violations were found" "$OUTPUT"
 }
 
 function test_phpcbf_returns_error_on_issues() {
-  OUTPUT="$(bin/phpcbf --no-colors --no-cache --suffix=.fixed --standard=tests/EndToEnd/Fixtures/phpcs.xml.dist tests/EndToEnd/Fixtures/ClassWithStyleError.inc)"
+  OUTPUT="$(bin/phpcbf --no-colors --no-cache --suffix=.fixed --standard=tests/EndToEnd/Fixtures/endtoend.xml.dist tests/EndToEnd/Fixtures/ClassWithStyleError.inc)"
   assert_exit_code 1
 
   assert_contains "F 1 / 1 (100%)" "$OUTPUT"
@@ -30,9 +30,9 @@ function test_phpcbf_bug_1112() {
   # See https://github.com/PHPCSStandards/PHP_CodeSniffer/issues/1112
   if [[ "$(uname)" == "Darwin" ]]; then
     # Perform some magic with `& fg` to prevent the processes from turning into a background job.
-    assert_successful_code "$(bash -ic 'bash --init-file <(echo "echo \"Subprocess\"") -c "bin/phpcbf --no-cache --parallel=2 --standard=tests/EndToEnd/Fixtures/phpcs.xml.dist tests/EndToEnd/Fixtures/ClassOneWithoutStyleError.inc tests/EndToEnd/Fixtures/ClassTwoWithoutStyleError.inc" & fg')"
+    assert_successful_code "$(bash -ic 'bash --init-file <(echo "echo \"Subprocess\"") -c "bin/phpcbf --no-cache --parallel=2 --standard=tests/EndToEnd/Fixtures/endtoend.xml.dist tests/EndToEnd/Fixtures/ClassOneWithoutStyleError.inc tests/EndToEnd/Fixtures/ClassTwoWithoutStyleError.inc" & fg')"
   else
     # This is not needed on Linux / GitHub Actions
-    assert_successful_code "$(bash -ic 'bash --init-file <(echo "echo \"Subprocess\"") -c "bin/phpcbf --no-cache --parallel=2 --standard=tests/EndToEnd/Fixtures/phpcs.xml.dist tests/EndToEnd/Fixtures/ClassOneWithoutStyleError.inc tests/EndToEnd/Fixtures/ClassTwoWithoutStyleError.inc"')"
+    assert_successful_code "$(bash -ic 'bash --init-file <(echo "echo \"Subprocess\"") -c "bin/phpcbf --no-cache --parallel=2 --standard=tests/EndToEnd/Fixtures/endtoend.xml.dist tests/EndToEnd/Fixtures/ClassOneWithoutStyleError.inc tests/EndToEnd/Fixtures/ClassTwoWithoutStyleError.inc"')"
   fi
 }
--- tests/EndToEnd/phpcs_test.sh
+++ tests/EndToEnd/phpcs_test.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 function test_phpcs_is_working() {
-  assert_successful_code "$(bin/phpcs --no-cache --standard=tests/EndToEnd/Fixtures/phpcs.xml.dist tests/EndToEnd/Fixtures/ClassOneWithoutStyleError.inc tests/EndToEnd/Fixtures/ClassTwoWithoutStyleError.inc)"
+  assert_successful_code "$(bin/phpcs --no-cache --standard=tests/EndToEnd/Fixtures/endtoend.xml.dist tests/EndToEnd/Fixtures/ClassOneWithoutStyleError.inc tests/EndToEnd/Fixtures/ClassTwoWithoutStyleError.inc)"
 }
 
 function test_phpcs_is_working_in_parallel() {
-  assert_successful_code "$(bin/phpcs --no-cache --parallel=2 --standard=tests/EndToEnd/Fixtures/phpcs.xml.dist tests/EndToEnd/Fixtures/ClassOneWithoutStyleError.inc tests/EndToEnd/Fixtures/ClassTwoWithoutStyleError.inc)"
+  assert_successful_code "$(bin/phpcs --no-cache --parallel=2 --standard=tests/EndToEnd/Fixtures/endtoend.xml.dist tests/EndToEnd/Fixtures/ClassOneWithoutStyleError.inc tests/EndToEnd/Fixtures/ClassTwoWithoutStyleError.inc)"
 }
 
 function test_phpcs_returns_error_on_issues() {
-  OUTPUT="$(bin/phpcs --no-colors --no-cache --standard=tests/EndToEnd/Fixtures/phpcs.xml.dist tests/EndToEnd/Fixtures/ClassWithStyleError.inc)"
+  OUTPUT="$(bin/phpcs --no-colors --no-cache --standard=tests/EndToEnd/Fixtures/endtoend.xml.dist tests/EndToEnd/Fixtures/ClassWithStyleError.inc)"
   assert_exit_code 2
 
   assert_contains "E 1 / 1 (100%)" "$OUTPUT"
@@ -20,9 +20,9 @@ function test_phpcs_bug_1112() {
   # See https://github.com/PHPCSStandards/PHP_CodeSniffer/issues/1112
     if [[ "$(uname)" == "Darwin" ]]; then
       # Perform some magic with `& fg` to prevent the processes from turning into a background job.
-      assert_successful_code "$(bash -ic 'bash --init-file <(echo "echo \"Subprocess\"") -c "bin/phpcs --no-cache --parallel=2 --standard=tests/EndToEnd/Fixtures/phpcs.xml.dist tests/EndToEnd/Fixtures/ClassOneWithoutStyleError.inc tests/EndToEnd/Fixtures/ClassTwoWithoutStyleError.inc" & fg')"
+      assert_successful_code "$(bash -ic 'bash --init-file <(echo "echo \"Subprocess\"") -c "bin/phpcs --no-cache --parallel=2 --standard=tests/EndToEnd/Fixtures/endtoend.xml.dist tests/EndToEnd/Fixtures/ClassOneWithoutStyleError.inc tests/EndToEnd/Fixtures/ClassTwoWithoutStyleError.inc" & fg')"
     else
       # This is not needed on Linux / GitHub Actions
-      assert_successful_code "$(bash -ic 'bash --init-file <(echo "echo \"Subprocess\"") -c "bin/phpcs --no-cache --parallel=2 --standard=tests/EndToEnd/Fixtures/phpcs.xml.dist tests/EndToEnd/Fixtures/ClassOneWithoutStyleError.inc tests/EndToEnd/Fixtures/ClassTwoWithoutStyleError.inc"')"
+      assert_successful_code "$(bash -ic 'bash --init-file <(echo "echo \"Subprocess\"") -c "bin/phpcs --no-cache --parallel=2 --standard=tests/EndToEnd/Fixtures/endtoend.xml.dist tests/EndToEnd/Fixtures/ClassOneWithoutStyleError.inc tests/EndToEnd/Fixtures/ClassTwoWithoutStyleError.inc"')"
     fi
 }
