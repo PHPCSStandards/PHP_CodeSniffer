e23fdf965947a851e750a691dee1c524e5357e16
jrfnl <jrfnl@users.noreply.github.com>
2025-08-06 17:20:53 +0200

    E2E tests: rename fixtures directory
    ... to be in line with other test fixture directories.

--- tests/EndToEnd/phpcbf_test.sh
+++ tests/EndToEnd/phpcbf_test.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 
 function tear_down() {
-  rm -r tests/EndToEnd/Files/*.fixed
+  rm -r tests/EndToEnd/Fixtures/*.fixed
 }
 
 function test_phpcbf_is_working() {
-  OUTPUT="$(bin/phpcbf --no-cache --standard=tests/EndToEnd/Files/phpcs.xml.dist tests/EndToEnd/Files/ClassOneWithoutStyleError.inc tests/EndToEnd/Files/ClassTwoWithoutStyleError.inc)"
+  OUTPUT="$(bin/phpcbf --no-cache --standard=tests/EndToEnd/Fixtures/phpcs.xml.dist tests/EndToEnd/Fixtures/ClassOneWithoutStyleError.inc tests/EndToEnd/Fixtures/ClassTwoWithoutStyleError.inc)"
 
   assert_successful_code
   assert_contains "No violations were found" "$OUTPUT"
 }
 
 function test_phpcbf_is_working_in_parallel() {
-  OUTPUT="$(bin/phpcbf --no-cache --parallel=2 --standard=tests/EndToEnd/Files/phpcs.xml.dist tests/EndToEnd/Files/ClassOneWithoutStyleError.inc tests/EndToEnd/Files/ClassTwoWithoutStyleError.inc)"
+  OUTPUT="$(bin/phpcbf --no-cache --parallel=2 --standard=tests/EndToEnd/Fixtures/phpcs.xml.dist tests/EndToEnd/Fixtures/ClassOneWithoutStyleError.inc tests/EndToEnd/Fixtures/ClassTwoWithoutStyleError.inc)"
 
   assert_successful_code
   assert_contains "No violations were found" "$OUTPUT"
 }
 
 function test_phpcbf_returns_error_on_issues() {
-  OUTPUT="$(bin/phpcbf --no-colors --no-cache --suffix=.fixed --standard=tests/EndToEnd/Files/phpcs.xml.dist tests/EndToEnd/Files/ClassWithStyleError.inc)"
+  OUTPUT="$(bin/phpcbf --no-colors --no-cache --suffix=.fixed --standard=tests/EndToEnd/Fixtures/phpcs.xml.dist tests/EndToEnd/Fixtures/ClassWithStyleError.inc)"
   assert_exit_code 1
 
   assert_contains "F 1 / 1 (100%)" "$OUTPUT"
@@ -30,9 +30,9 @@ function test_phpcbf_bug_1112() {
   # See https://github.com/PHPCSStandards/PHP_CodeSniffer/issues/1112
   if [[ "$(uname)" == "Darwin" ]]; then
     # Perform some magic with `& fg` to prevent the processes from turning into a background job.
-    assert_successful_code "$(bash -ic 'bash --init-file <(echo "echo \"Subprocess\"") -c "bin/phpcbf --no-cache --parallel=2 --standard=tests/EndToEnd/Files/phpcs.xml.dist tests/EndToEnd/Files/ClassOneWithoutStyleError.inc tests/EndToEnd/Files/ClassTwoWithoutStyleError.inc" & fg')"
+    assert_successful_code "$(bash -ic 'bash --init-file <(echo "echo \"Subprocess\"") -c "bin/phpcbf --no-cache --parallel=2 --standard=tests/EndToEnd/Fixtures/phpcs.xml.dist tests/EndToEnd/Fixtures/ClassOneWithoutStyleError.inc tests/EndToEnd/Fixtures/ClassTwoWithoutStyleError.inc" & fg')"
   else
     # This is not needed on Linux / GitHub Actions
-    assert_successful_code "$(bash -ic 'bash --init-file <(echo "echo \"Subprocess\"") -c "bin/phpcbf --no-cache --parallel=2 --standard=tests/EndToEnd/Files/phpcs.xml.dist tests/EndToEnd/Files/ClassOneWithoutStyleError.inc tests/EndToEnd/Files/ClassTwoWithoutStyleError.inc"')"
+    assert_successful_code "$(bash -ic 'bash --init-file <(echo "echo \"Subprocess\"") -c "bin/phpcbf --no-cache --parallel=2 --standard=tests/EndToEnd/Fixtures/phpcs.xml.dist tests/EndToEnd/Fixtures/ClassOneWithoutStyleError.inc tests/EndToEnd/Fixtures/ClassTwoWithoutStyleError.inc"')"
   fi
 }
