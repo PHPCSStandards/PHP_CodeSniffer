00a694b255c031ca379e27deee21c934d9dbcadd
jrfnl <jrfnl@users.noreply.github.com>
2025-08-11 19:29:16 +0200

    PHP 8.4 | Squiz/DocCommentAlignment: add tests with abstract property

--- src/Standards/Squiz/Tests/Commenting/DocCommentAlignmentUnitTest.php
+++ src/Standards/Squiz/Tests/Commenting/DocCommentAlignmentUnitTest.php
@@ -68,6 +68,13 @@ final class DocCommentAlignmentUnitTest extends AbstractSniffUnitTest
             $errors[121] = 1;
             $errors[125] = 1;
             $errors[126] = 1;
+
+            $errors[136] = 1;
+            $errors[137] = 1;
+            $errors[141] = 2;
+            $errors[142] = 1;
+            $errors[143] = 1;
+            $errors[144] = 1;
         }//end if
 
         return $errors;
