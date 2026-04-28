06ac35aa79bcbcdad562a7ce1675bb27e372291f
Jan Nedbal <janedbal@gmail.com>
2026-04-28 15:43:58 +0200

    Runner: distribute files round-robin by size in parallel mode

--- src/Runner.php
+++ src/Runner.php
@@ -20,6 +20,7 @@ use PHP_CodeSniffer\Exceptions\RuntimeException;
 use PHP_CodeSniffer\Files\DummyFile;
 use PHP_CodeSniffer\Files\File;
 use PHP_CodeSniffer\Files\FileList;
+use PHP_CodeSniffer\Files\LocalFile;
 use PHP_CodeSniffer\Util\Cache;
 use PHP_CodeSniffer\Util\Common;
 use PHP_CodeSniffer\Util\ExitCode;
@@ -397,18 +398,37 @@ class Runner
             }
         } else {
             // Batching and forking.
-            $childProcs  = [];
-            $numPerBatch = ceil($numFiles / $this->config->parallel);
+            $childProcs = [];
 
-            for ($batch = 0; $batch < $this->config->parallel; $batch++) {
-                $startAt = ($batch * $numPerBatch);
-                if ($startAt >= $numFiles) {
-                    break;
+            // Distribute files round-robin in size order so each batch
+            // ends up with a similar total workload.
+            $sortedPaths = [];
+            $todo->rewind();
+            while ($todo->valid() === true) {
+                $sortedPaths[] = $todo->key();
+                $todo->next();
+            }
+
+            $sizes = [];
+            foreach ($sortedPaths as $path) {
+                $size = @filesize($path);
+                if ($size === false) {
+                    $size = 0;
                 }
 
-                $endAt = ($startAt + $numPerBatch);
-                if ($endAt > $numFiles) {
-                    $endAt = $numFiles;
+                $sizes[$path] = $size;
+            }
+
+            usort(
+                $sortedPaths,
+                static function ($a, $b) use ($sizes) {
+                    return ($sizes[$b] - $sizes[$a]);
+                }
+            );
+
+            for ($batch = 0; $batch < $this->config->parallel; $batch++) {
+                if ($batch >= $numFiles) {
+                    break;
                 }
 
                 $childOutFilename = tempnam(sys_get_temp_dir(), 'phpcs-child');
@@ -418,12 +438,6 @@ class Runner
                 } elseif ($pid !== 0) {
                     $childProcs[$pid] = $childOutFilename;
                 } else {
-                    // Move forward to the start of the batch.
-                    $todo->rewind();
-                    for ($i = 0; $i < $startAt; $i++) {
-                        $todo->next();
-                    }
-
                     // Reset the reporter to make sure only figures from this
                     // file batch are recorded.
                     $this->reporter->totalFiles           = 0;
@@ -437,12 +451,11 @@ class Runner
                     // Process the files.
                     $pathsProcessed = [];
                     ob_start();
-                    for ($i = $startAt; $i < $endAt; $i++) {
-                        $path = $todo->key();
-                        $file = $todo->current();
+                    for ($i = $batch; $i < $numFiles; $i += $this->config->parallel) {
+                        $path = $sortedPaths[$i];
+                        $file = new LocalFile($path, $this->ruleset, $this->config);
 
                         if ($file->ignored === true) {
-                            $todo->next();
                             continue;
                         }
 
@@ -458,7 +471,6 @@ class Runner
                         $this->processFile($file);
 
                         $pathsProcessed[] = $path;
-                        $todo->next();
                     }
 
                     $debugOutput = ob_get_contents();
