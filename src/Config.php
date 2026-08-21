368d08340a56ca54c213761c85a70a03cea95d60
jrfnl <jrfnl@users.noreply.github.com>
2026-08-22 00:11:12 +0200

    Config: update version nr to (lower) next

--- src/Config.php
+++ src/Config.php
@@ -90,7 +90,7 @@ class Config
      *
      * @var string
      */
-    public const VERSION = '4.1.0';
+    public const VERSION = '4.0.5';
 
     /**
      * Package stability; either stable, RC, beta or alpha.
